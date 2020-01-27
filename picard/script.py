a4ce7e52a412910370dee67e84917e95cfdb7dfd
tdiaz <tdiaz@apple2.org>
2020-01-27 05:51:06 -0800

    A Modification to Add a $titlecase script function.
    This:
    "the word is EAT THIS NOW. USA. USB. US VS us. the big bad fire truck is going to eat you! abba & ac/dc are playiNG on a Farm down by the river"
    
    Becomes:
    The Word Is Eat This Now. Usa. Usb. US vs Us. The Big Bad Fire Truck Is Going to Eat You! ABBA & AC/DC Are Playing on a Farm Down by the River.

--- picard/script.py
+++ picard/script.py
@@ -20,11 +20,15 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from __future__ import unicode_literals  # amd
+
+
 from collections import namedtuple
 from functools import reduce
 from inspect import getfullargspec
 import operator
 import re
+import sys #  amd
 import unicodedata
 
 from picard import config
@@ -34,33 +38,52 @@ from picard.metadata import (
 )
 from picard.plugin import ExtensionPoint
 from picard.util import uniqify
+# amd
 
+SMALL = 'a|an|and|as|at|but|by|en|for|feat|if|in|of|on|or|the|to|v\.?|via|vs\.?'
+PUNCT = r"""!"“#$%&'‘()*+,\-–‒—―./:;?@[\\\]_`{|}~"""
 
-class ScriptError(Exception):
+SMALL_WORDS = re.compile(r'^(%s)$' % SMALL, re.I)
+INLINE_PERIOD = re.compile(r'[a-z][.][a-z]', re.I)
+UC_ELSEWHERE = re.compile(r'[%s]*?[a-zA-Z]+[A-Z]+?' % PUNCT)
+CAPFIRST = re.compile(r"^[%s]*?([A-Za-z])" % PUNCT)
+SMALL_FIRST = re.compile(r'^([%s]*)(%s)\b' % (PUNCT, SMALL), re.I)
+SMALL_LAST = re.compile(r'\b(%s)[%s]?$' % (SMALL, PUNCT), re.I)
+SUBPHRASE = re.compile(r'([:.;?!\-–‒—―][ ])(%s)' % SMALL)
+APOS_SECOND = re.compile(r"^[dol]{1}['‘]{1}[a-z]+(?:['s]{2})?$", re.I)
+UC_INITIALS = re.compile(r"^(?:[A-Z]{1}\.{1}|[A-Z]{1}\.{1}[A-Z]{1})+$")
+MAC_MC = re.compile(r"^([Mm]c|MC)(\w.+)")
+
+class Immutable(object):
     pass
 
+text_type = unicode if sys.version_info < (3,) else str
+
+class ImmutableString(text_type, Immutable):
+    pass
+
+class ImmutableBytes(bytes, Immutable):
+    pass
+# amd
+class ScriptError(Exception):
+    pass
 
 class ScriptParseError(ScriptError):
     pass
 
-
 class ScriptEndOfFile(ScriptParseError):
     pass
-
 
 class ScriptSyntaxError(ScriptParseError):
     pass
 
-
 class ScriptUnknownFunction(ScriptError):
     pass
-
 
 class ScriptText(str):
 
     def eval(self, state):
         return self
-
 
 def normalize_tagname(name):
     if name.startswith('_'):
@@ -884,7 +907,133 @@ def func_title(parser, text):
             capital = False
         capitalized += t
     return capitalized
+# amd - From Here .....
 
+
+def _mark_immutable(text):
+    if isinstance(text, bytes):
+        return ImmutableBytes(text)
+    return ImmutableString(text)
+
+
+def set_small_word_list(small=SMALL):
+    global SMALL_WORDS
+    global SMALL_FIRST
+    global SMALL_LAST
+    global SUBPHRASE
+    SMALL_WORDS = re.compile(r'^(%s)$' % small, re.I)
+    SMALL_FIRST = re.compile(r'^([%s]*)(%s)\b' % (PUNCT, small), re.I)
+    SMALL_LAST = re.compile(r'\b(%s)[%s]?$' % (small, PUNCT), re.I)
+    SUBPHRASE = re.compile(r'([:.;?!][ ])(%s)' % small)
+
+
+def keepwords(word, **kwargs):
+    if word.upper() in ('USA', 'US', 'ABBA', 'AC/DC', 'The Who', 'The Beatles'):
+        return word.upper()
+
+def smart_title(s):
+    return ' '.join(w if w.isupper() else w.capitalize() for w in s.split())
+
+def func_titlecase(parser, text, callback=keepwords, small_first_last=True):
+    """
+    Original Perl version by: John Gruber http://daringfireball.net/ 10 May 2008
+    Python version by Stuart Colville http://muffinresearch.co.uk
+    License: http://www.opensource.org/licenses/mit-license.php
+    """
+    """
+    Titlecases input text
+    This filter changes all words to Title Caps, and attempts to be clever
+    about *un*capitalizing SMALL words like a/an/the in the input.
+    The list of "SMALL words" which are not capped comes from
+    the New York Times Manual of Style, plus 'vs' and 'v'.
+    """
+
+    lines = re.split('[\r\n]+', text)
+    processed = []
+    for line in lines:
+        all_caps = line.upper() == line
+        words = re.split('[\t ]', line)
+        tc_line = []
+        for word in words:
+            if callback:
+                new_word = callback(word, all_caps=all_caps)
+                if new_word:
+                    # Address #22: If a callback has done something
+                    # specific, leave this string alone from now on
+                    tc_line.append(_mark_immutable(new_word))
+                    continue
+
+            if all_caps:
+                if UC_INITIALS.match(word):
+                    tc_line.append(word)
+                    continue
+
+            if APOS_SECOND.match(word):
+                if len(word[0]) == 1 and word[0] not in 'aeiouAEIOU':
+                    word = word[0].lower() + word[1] + word[2].upper() + word[3:]
+                else:
+                    word = word[0].upper() + word[1] + word[2].upper() + word[3:]
+                tc_line.append(word)
+                continue
+
+            match = MAC_MC.match(word)
+            if match:
+                tc_line.append("%s%s" % (match.group(1).capitalize(),
+                                         titlecase(match.group(2),callback,small_first_last)))
+                continue
+
+            if INLINE_PERIOD.search(word) or (not all_caps and UC_ELSEWHERE.match(word)):
+                tc_line.append(word)
+                continue
+            if SMALL_WORDS.match(word):
+                tc_line.append(word.lower())
+                continue
+
+            if "/" in word and "//" not in word:
+                slashed = map(
+                    lambda t: titlecase(t,callback,False),
+                    word.split('/')
+                )
+                tc_line.append("/".join(slashed))
+                continue
+
+            if '-' in word:
+                hyphenated = map(
+                    lambda t: titlecase(t,callback,small_first_last),
+                    word.split('-')
+                )
+                tc_line.append("-".join(hyphenated))
+                continue
+
+            if all_caps:
+                word = word.lower()
+
+            # Just a normal word that needs to be capitalized
+            tc_line.append(CAPFIRST.sub(lambda m: m.group(0).upper(), word))
+
+        if small_first_last and tc_line:
+            if not isinstance(tc_line[0], Immutable):
+                tc_line[0] = SMALL_FIRST.sub(lambda m: '%s%s' % (
+                    m.group(1),
+                    m.group(2).capitalize()
+                ), tc_line[0])
+
+            if not isinstance(tc_line[-1], Immutable):
+                tc_line[-1] = SMALL_LAST.sub(
+                    lambda m: m.group(0).capitalize(), tc_line[-1]
+                )
+
+        result = " ".join(tc_line)
+
+        result = SUBPHRASE.sub(lambda m: '%s%s' % (
+            m.group(1),
+            m.group(2).capitalize()
+        ), result)
+
+        processed.append(result)
+
+    return "\n".join(processed)
+# amd ... to Here.
 
 def iswbound(char):
     # GPL 2.0 licensed code by Javier Kohen, Sambhav Kothari
@@ -967,3 +1116,4 @@ register_script_function(func_ne_any, "ne_any", check_argcount=False)
 register_script_function(func_title, "title")
 register_script_function(func_is_audio, "is_audio")
 register_script_function(func_is_video, "is_video")
+register_script_function(func_titlecase, "titlecase")
